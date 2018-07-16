7104e84fc304b28c833d98e08117a4cb1a39d45e
Hoang <nhathoang10217@gmail.com>
2018-07-16 10:34:41 +0700

    404 page

--- wp-content/themes/jayahr/404.php
+++ wp-content/themes/jayahr/404.php
@@ -5,5 +5,8 @@
  * @author NLS Team
  */
 get_header();?>
-
+	<div class="container">
+		<img src="<?php echo get_stylesheet_directory_uri().'/images/404.png'; ?>" alt="404">
+		<a class="read-more" href="<?php echo home_url(); ?>">Back to home</a>
+	</div>
 <?php get_footer();
